d94582fab83f02c4e6851e994c6b65a8ec883b16
Jeekagee <kageena.arunthavarasa@kodplex.com>
2022-05-08 21:36:24 +0530

    first

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,7 @@
 
     <title>DS Office</title>
 
-    <!-- Fonts -->
+    <!-- Fonts hg -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
